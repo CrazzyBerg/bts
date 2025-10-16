76725d47bb931e44979320a2e92cef9c440ec032
kova1enko.n <kovalenko.n@ajax.systems>
2025-10-16 10:49:47 +0300

    fix nftables

--- update_nft.sh
+++ update_nft.sh
@@ -4,9 +4,9 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo sysctl -p
 
 sudo nft add table ip nat
-sudo nft add chain ip nat postrouting { type nat hook postrouting priority srcnat; policy accept; }
+sudo nft add chain ip nat postrouting '{ type nat hook postrouting priority srcnat; policy accept; }'
 sudo nft add rule ip nat postrouting oifname "eth0" masquerade
 
-sudo nft list ruleset > /etc/nftables.conf
+sudo sh -c 'nft list ruleset > /etc/nftables.conf'
 sudo systemctl enable nftables
 sudo systemctl restart nftables
