196012dd29e7960e0ccfb8dc739d82a61cf441f2
kova1enko.n <kovalenko.n@ajax.systems>
2026-06-11 08:49:35 +0300

    nft by wan

--- update_nft.sh
+++ update_nft.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+WAN_IF=$(ip route | awk '/default/ {print $5; exit}')
+
 sudo sed -i 's/^#*net.ipv4.ip_forward=.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf && sudo sysctl -p
 
 sudo nft flush ruleset
@@ -10,9 +12,11 @@ flush ruleset
 table ip nat {
     chain postrouting {
         type nat hook postrouting priority srcnat; policy accept;
-        oifname "eth0" masquerade
+        oifname "$WAN_IF" masquerade
     }
 }
 EOF
 
+sudo nft -f /etc/nftables.conf
 sudo systemctl enable nftables
+sudo systemctl restart nftables
